8e0a286990631882feea85b669c7f2a49ddbe001
Laptop Wala <muhammadomairakbar@gmail.com>
2024-07-20 15:25:02 +0500

    -

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -21,8 +21,7 @@ const Photo = () => {
                     className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
                     fill="transparent"
                     viewBox="0 0 506 506"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
+                    xmlns="http://www.w3.org/2000/svg">
                     <motion.circle
                         cx="253"
                         cy="253"
